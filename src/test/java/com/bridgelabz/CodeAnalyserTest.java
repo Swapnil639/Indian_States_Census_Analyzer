4d1ab57349a0d3a0da5dc72024edb7993b0a9271
Swapnil639 <swapnilpatil631997@gmail.com>
2023-05-14 13:33:08 +0530

    Added :welcome message

---
+++ src/test/java/com/bridgelabz/CodeAnalyserTest.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class CodeAnalyserTest {
+    public static void main(String[] args) {
+        System.out.println("Indian state census analyser problem");
+    }
+}
